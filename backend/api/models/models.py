55f4ed2cae444956a41f59067ef399d91ee642ff
Bessong Junior <juniorbesong8@hotmail.com>
2023-07-24 09:03:03 +0100

    rename user to customer

--- backend/api/models/models.py
+++ backend/api/models/models.py
@@ -12,15 +12,28 @@ class Admin(db.Model):
     id: int = db.Column(db.Integer, primary_key=True)
     first_name: str = db.Column(db.String, nullable=False)
     last_name: str = db.Column(db.String, nullable=False)
-    username: str = db.Column(db.String, nullable=False)
-    email: str = db.Column(db.String, nullable=False)
-    password: str = db.Column(db.String, nullable=False, unique=False)
+    username: str = db.Column(db.String, nullable=False, unique=True)
+    email: str = db.Column(db.String, nullable=False, unique=True)
+    password: str = db.Column(db.String, nullable=True, unique=False)
     date_joined: datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    date_modified: datetime = db.Column(db.DateTime, nullable=False)
+    date_modified: datetime = db.Column(db.DateTime, nullable=True)
+    image_file: str = db.Column(db.String, nullable=True, default='avatar.jpg')
+    jwt_auth_active = db.Column(db.Boolean())
+    is_admin = db.Column(db.Boolean(), default=True)
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return f"Admin {self.username}"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
     
 
-class User(db.Model):
+class Customer(db.Model):
     '''This represent our user table in db'''
     id: int = db.Column(db.Integer, primary_key=True)
     # add user login including google|facebook oauth inclusive.
@@ -32,9 +45,18 @@ class Product(db.Model):
     id: int = db.Column(db.Integer, primary_key=True)
     name: str = db.Column(db.String, nullable=False, unique=True)
     description: str = db.Column(db.Text)
+    price: str = 0
+    currency: str = ''
+    info: str = ''
+    full_description: str = ''
+    slug: str   = '' 
+    img_main: str   = ''
+    img_card: str = ''
+    img_1: str = ''
+    img_2: str = ''
+    img_3: str = ''
     # product cat
     # product brand
-    # images
 
     
 
